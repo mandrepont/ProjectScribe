62a0c7b9a908a23fc5f34ca129521eb0cfbc4b50
Michael Andrepont <andrepontmichael@gmail.com>
2017-04-20 18:53:41 -0500

    Added Note Controller Testing

---
+++ ScribeTest/ServerTest/NoteControllerTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using ScribeServer.Controllers;
+using ScribeServer.Models;
+
+namespace ScribeTest.ServerTest
+{
+    /// <summary>
+    /// Class meant to test the controller of the note
+    /// not the routing.
+    /// </summary>
+    public class NoteControllerTest
+    {
+        private IList<Note> AddedNotes;
+        private NotesController controller;
+
+        /// <summary>
+        /// For this constructer we do not get live data so we must make the database in a mock way.
+        /// </summary>
+        public NoteControllerTest()
+        {
+            controller = new NotesController();
+            AddedNotes = new List<Note>();
+            int numOfNotes = 3;
+            Database.GlobalDatabase = new Database();
+            Database.GlobalDatabase.Notes = new List<Note>();
+            for (int i = 0; i <= numOfNotes; i++)
+            {
+                var note = new Note()
+                {
+                    Id = i,
+                    AuthorId = i,
+                    Title = "Note Num: " + i,
+                    Content = "Content test Num" + i,
+                    AuthorName = "Name " + i
+                };
+                
+                Database.GlobalDatabase.Notes.Add(note);
+                AddedNotes.Add(note);
+            }
+        }
+        /// <summary>
+        /// Gets all of the current notes int the mock DB
+        /// Ensures valid response.
+        /// </summary>
+        [Fact]
+        public void GetAllTest()
+        {
+            var gotNotes = controller.Get();
+            Assert.Equal(AddedNotes, gotNotes);
+        }
+
+        /// <summary>
+        /// Test if the notes are really got out of the database.
+        /// Checks a few with one that does not exist.
+        /// </summary>
+        /// <param name="i">id of note.</param>
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void GetIdTest(int i)
+        {
+            var note = controller.Get(i);
+            if (i != 3)
+                Assert.Equal(AddedNotes[i], note);
+            else
+                Assert.NotNull(note);
+        }
+
+        /// <summary>
+        /// Post some data and make sure that data is really there. 
+        /// </summary>
+        [Fact]
+        public void PostTest()
+        {
+            var note = new Note
+            {
+                AuthorId = 3,
+                Content= "Content 3",
+                Title = "PostedNote",
+                AuthorName = "Name 3",
+            };
+            controller.Post(note);
+            Assert.NotNull(Database.GlobalDatabase.Notes.First(n => n.Title == note.Title));
+        }
+
+        /// <summary>
+        /// Edits the 3rd note and gets it out the database.
+        /// </summary>
+        [Fact]
+        public void PutTest()
+        {
+            AddedNotes[2].AuthorName = "UpdatedName";
+            controller.Put(2, AddedNotes[2]);
+            Assert.NotNull(Database.GlobalDatabase.Notes.First(n => n.AuthorName == "UpdatedName"));
+        }
+        /// <summary>
+        /// Deletes one of the added notes And then test to see if it is really deleted.
+        /// </summary>
+        [Fact]
+        public void DeleteTest()
+        {
+            controller.Delete(AddedNotes[1].Id);
+            Assert.False(Database.GlobalDatabase.Notes.Contains(AddedNotes[1]));
+        }
+    }
+}
---
+++ ScribeTest/ServerTest/NoteHanlerTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ScribeServer.Data;
+using ScribeServer.Models;
+using Xunit;
+
+namespace ScribeTest.ServerTest
+{
+    ///// <summary>
+    ///// While the name might be missleading, the database is also tested via this test. 
+    ///// </summary>
+    //public class NoteHanlerTest
+    //{
+    //    /// <summary>
+    //    /// Called before any test so that we can create the database.
+    //    /// </summary>
+    //    public NoteHanlerTest()
+    //    {
+    //        Database.CheckDatabase();
+    //    }
+
+    //    /// <summary>
+    //    /// Adds a new note to the database.
+    //    /// </summary>
+    //    [Fact]
+    //    public void AddTest()
+    //    {
+    //        var note = new Note()
+    //        {
+    //            AuthorName = "Michael",
+    //            Title = "Testing out a title",
+    //            Content = "Test filler content"
+    //        };
+    //        NoteHandler.AddNote(note);
+    //        var retrievedNote = Database.GlobalDatabase.Notes.FirstOrDefault(s => s.Id == Database.GlobalDatabase.LastNoteID);
+    //        Assert.NotNull(retrievedNote);
+    //        Assert.Equal(note.AuthorName, retrievedNote.AuthorName);
+    //        Assert.Equal(note.Content, retrievedNote.Content);
+    //        Assert.Equal(note.Title, retrievedNote.Title);
+    //    }
+
+    //    /// <summary>
+    //    /// Add note must be ran before this test.
+    //    /// Check if the note being updated really is updated.
+    //    /// </summary>
+    //    [Fact]
+    //    public void UpdateTest()
+    //    {
+    //        var retrievedNote = Database.GlobalDatabase.Notes.FirstOrDefault(s => s.Id == Database.GlobalDatabase.LastNoteID);
+    //        var newNote = retrievedNote;
+    //        newNote.Title = "Yep edited note.";
+    //        Assert.False(!NoteHandler.UpdateNote(newNote));
+    //    }
+    //    /// <summary>
+    //    /// Only works if the add test was ran before this.
+    //    /// Removes the added note and check if it was really removed. 
+    //    /// </summary>
+    //    [Fact]
+    //    public void RemoveTest()
+    //    {
+    //        var retrievedNote = Database.GlobalDatabase.Notes.FirstOrDefault(s => s.Id == Database.GlobalDatabase.LastNoteID);
+    //        Assert.False(!NoteHandler.RemoveNote(retrievedNote.Id));
+    //        Assert.NotNull(retrievedNote);
+    //        Assert.Null(Database.GlobalDatabase.Notes.FirstOrDefault(s => s.Id == Database.GlobalDatabase.LastNoteID));
+
+    //    }
+
+    //}
+}
